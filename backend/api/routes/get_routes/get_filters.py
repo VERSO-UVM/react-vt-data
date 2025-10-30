8f40e3f39451c29388c12a1dd31413f0b3848f4e
Fitz Koch <fw.j.keenan.koch@gmail.com>
2025-10-30 16:51:34 -0400

    added modal profile

--- backend/api/routes/get_routes/get_filters.py
+++ backend/api/routes/get_routes/get_filters.py
@@ -1,19 +1,17 @@
-
-from fastapi import  APIRouter
+from fastapi import APIRouter
 from app_utils import data_loading
 from app_utils.df_filtering import FilterState
 import logging
+
 logger = logging.getLogger(__name__)
 
 router = APIRouter()
 
+
 @router.get("/load/mapping/zoning/filters")
 async def read_zoning_data():
     data = data_loading.masterload(name="zoning")
-    filter_columns=["County", "Jurisdiction", "District Name"]
+    filter_columns = ["County", "Jurisdiction", "District Name"]
     logger.info(f"cols are {filter_columns}")
     Filter = FilterState(data, filter_columns=filter_columns)
-    return {
-        "tree": Filter.tree,
-        "labels": filter_columns
-    }
+    return {"tree": Filter.tree, "labels": filter_columns}
