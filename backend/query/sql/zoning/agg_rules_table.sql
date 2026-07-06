eb6c4d89c4ddabbb4edf315f47e89937e0ffd2f9
Isarge05 <iansarge05@gmail.com>
2026-07-06 10:03:31 -0400

    aggregated rules sql update

--- backend/query/sql/zoning/agg_rules_table.sql
+++ backend/query/sql/zoning/agg_rules_table.sql
@@ -2,9 +2,9 @@ SELECT
     r.use_type,
     r.val,
     SUM(i.Acres) AS Acres
-FROM main.zoning_rules AS r
-JOIN main.zoning_info AS i USING (OBJECT_ID)
-{{ where_string }} 
+FROM main.zoning_rules r
+JOIN main.zoning_info i USING (OBJECT_ID)
+{where_string} 
 AND r.rule = 'Allowance' AND i.District_Type IN ('Residential', 'Mixed')
 GROUP BY 
     r.use_type,
