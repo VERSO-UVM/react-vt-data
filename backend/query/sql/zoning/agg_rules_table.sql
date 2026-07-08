97342d24f8c4c48a9fa2bd211ed8d2c58e17068d
Fitz Koch <fw.j.keenan.koch@gmail.com>
2026-07-08 16:35:26 -0400

    small sql fix

--- backend/query/sql/zoning/agg_rules_table.sql
+++ backend/query/sql/zoning/agg_rules_table.sql
@@ -1,12 +1,14 @@
-SELECT 
+SELECT
     r.use_type,
     r.val,
     SUM(i.Acres) AS Acres
-FROM main.zoning_rules r
-JOIN main.zoning_info i USING (OBJECT_ID)
-{{ where_string }} 
-AND r.rule = 'Allowance' 
-AND i.District_Type IN ('Residential', 'Mixed')
-GROUP BY 
+FROM main.zoning_rules AS r
+INNER JOIN main.zoning_info AS i USING (OBJECT_ID)
+
+
+{{ where_string }}
+    AND r.rule = 'Allowance'
+    AND i.District_Type IN ('Residential', 'Mixed')
+GROUP BY
     r.use_type,
     r.val
--- backend/query/sql/zoning/rules_table.sql
+++ backend/query/sql/zoning/rules_table.sql
@@ -2,10 +2,11 @@ WITH filtered_info AS (
     SELECT DISTINCT OBJECT_ID FROM zoning_info
     {{ where_string }}
 )
+
 SELECT
     r.OBJECT_ID,
     r.use_type,
     r.rule,
     r.val
-FROM zoning_rules r
-JOIN filtered_info f USING (OBJECT_ID)
+FROM zoning_rules AS r
+INNER JOIN filtered_info USING (OBJECT_ID)
