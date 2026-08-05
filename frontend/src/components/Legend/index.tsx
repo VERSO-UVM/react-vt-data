808c38a42b124b5a7a5fce06fad23a752235c291
AtticusTarleton <atarleto@uvm.edu>
2026-08-05 13:24:30 -0400

    changed the list to a table

--- frontend/src/components/Legend/index.tsx
+++ frontend/src/components/Legend/index.tsx
@@ -14,10 +14,6 @@ export default function MapLegend({
   rgba = '[0,0,0,0]',
   test = 'testing',
 }: LegendTable) {
-  // super basic for loop below. will modify
-  // 1. Define your array of data
-  const fruits = ['Apple', 'Banana', 'Orange', 'Mango'];
-
   const test_brackets = test.split(/},\s*/);
   // const legend_string = JSON.parse(test_brackets);
   console.log(test_brackets); //I am confused why this prints null
@@ -34,36 +30,47 @@ export default function MapLegend({
 
   return (
     <div>
-      <h2>Legend</h2>
-      <ul>
-        <li>
-          {test_brackets[0]
-            .split(/:\s*/)[0]
-            .replaceAll('\"', '')
-            .replaceAll('\{', '')}{' '}
-          | Color
-        </li>
+      <h2>Legend Table</h2>
+      <table>
+        <tr>
+          {' '}
+          <th style={{ paddingRight: '15px' }}>
+            {test_brackets[0]
+              ?.split(/:\s*/)[0]
+              ?.replaceAll('\"', '')
+              ?.replaceAll('\{', '')}{' '}
+          </th>
+          <th>Color</th>
+        </tr>
 
         {test_brackets.map((items_in_jsons, index) => (
-          <li key={index}>
-            {items_in_jsons.split(/,\s*/)[0].split(/:\s*/)[1]},{' '}
-            <span
-              style={{
-                display: 'inline-block',
-                backgroundColor: items_in_jsons
-                  .split(/,\s*/)[1]
-                  .split(/:\s*/)[1]
-                  .replaceAll('"', ''),
-                //for some reason sometimes the above line does not work
-                // backgroundColor: 'blue',
-                width: '20px',
-                height: '10px',
-              }}
-            ></span>
-          </li>
+          <tr key={index}>
+            <td style={{ paddingRight: '15px' }}>
+              {items_in_jsons
+                ?.split(/,\s*/)[0]
+                ?.split(/:\s*/)[1]
+                ?.replaceAll('"', '')}
+            </td>
+            <td>
+              <span
+                style={{
+                  display: 'inline-block',
+                  backgroundColor: items_in_jsons
+                    ?.split(/,\s*/)[1]
+                    ?.split(/:\s*/)[1]
+                    ?.replaceAll('"', ''),
+                  // for some reason sometimes the above line does not work. I believe this is because the previous split does not
+                  // fully happen before the next split starts (or something like that), which is why i added the optional chaining
+                  // backgroundColor: 'blue',
+                  width: '20px',
+                  height: '10px',
+                }}
+              ></span>
+            </td>
+          </tr>
           // <li key={index}>{items_in_jsons}</li>
         ))}
-      </ul>
+      </table>
     </div>
   );
 }
