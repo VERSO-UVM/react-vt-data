7da004baf6258f6c857aaa0174951c871e44cdcd
Fitz Koch <fw.j.keenan.koch@gmail.com>
2026-08-07 13:36:20 -0400

    made changes to suit containerization

--- backend/query/acs5.py
+++ backend/query/acs5.py
@@ -13,9 +13,9 @@ from pathlib import Path
 import pandas as pd
 
 from api.models import FilterSource, RangeFilter
+from app_utils.sql_render import sql_filter_block
 from query.core_functions import filter_tree
 from query.processed_db import DB
-from sql_render import sql_filter_block
 
 logger = logging.getLogger(__name__)
 sql_path = Path(__file__).resolve().parent / "sql" / "acs5"
--- backend/query/wastewater.py
+++ backend/query/wastewater.py
@@ -13,7 +13,7 @@ from pathlib import Path
 import pandas as pd
 
 from api.models import FilterSource
-from sql_render import sql_filter_block
+from app_utils.sql_render import sql_filter_block
 from query.processed_db import DB
 
 logger = logging.getLogger(__name__)
