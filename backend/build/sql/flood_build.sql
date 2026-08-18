aeddc6ceab5b513ca183c07c2c6af4c689d81d20
Ethan Elbert <your-github-email@example.com>
2026-08-18 10:19:18 -0400

    Updated hazard build sql to include AR_REVERT, bumped VERSION_ID out of info table, added info about what columns are

--- backend/build/sql/flood_build.sql
+++ backend/build/sql/flood_build.sql
@@ -2,7 +2,6 @@ CREATE OR REPLACE TABLE hazard AS
 SELECT
     i.id,
     i.DFIRM_ID,
-    i.VERSION_ID,
     i.FLD_AR_ID,
     i.STUDY_TYP,
     i.FLD_ZONE,
@@ -15,9 +14,6 @@ SELECT
     i.VELOCITY,
     i.VEL_UNIT,
     i.AR_REVERT,
-    i.AR_SUBTRV,
-    i.BFE_REVERT,
-    i.DEP_REVERT,
     i.DUAL_ZONE,
     i.SOURCE_CIT,
     g.geometry AS geom
