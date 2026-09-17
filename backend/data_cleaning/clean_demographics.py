f4df4876be14bac97a6d8f884776e7a58874163d
Isarge05 <iansarge05@gmail.com>
2026-09-17 14:25:25 -0400

    standardized geographic identifier cleaning in acs5 demographics, economic, housing, education, and snapshot tables

--- backend/data_cleaning/clean_demographics.py
+++ backend/data_cleaning/clean_demographics.py
@@ -7,7 +7,7 @@
     Data cleaning script for the raw (curated variables) `demographics`
     table in the DuckLake
 **Run with**:
-python -m data_cleaning.clean_demographics`
+python -m data_cleaning.clean_demographics
 """
 
 import duckdb
@@ -25,10 +25,101 @@ def read_raw_data(con: duckdb.DuckDBPyConnection) -> pd.DataFrame:
     return raw_df
 
 
+def clean_geo_type(df: pd.DataFrame) -> pd.DataFrame:
+    df["geo_type"] = df["geo_type"].replace("county_subdivision", "town")
+
+    return df
+
+
+def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
+    df.rename(
+        columns={
+            "Jurisdiction": "town",
+            "county": "county_fips",
+            "County_1": "county",
+            "state": "state_fips",
+            "NAME": "name",
+        },
+        inplace=True,
+    )
+
+    df["county_fips"] = df["state_fips"].astype("string") + df["county_fips"].astype(
+        "string"
+    )
+    df.drop(columns=["state_fips"], inplace=True)
+
+    return df
+
+
+def add_geoid(con: duckdb.DuckDBPyConnection, df: pd.DataFrame) -> pd.DataFrame:
+    df_with_geoid = con.execute(
+        """--sql
+        SELECT
+            df.*,
+            town_geoids.GEOID AS GEOID,
+            county_names.CNTYNAME AS county_name
+        FROM df
+        LEFT JOIN lake.RAW.vt_town_lines AS town_geoids
+        ON df.town = TRIM(SPLIT_PART(town_geoids.NAME, ',', 1))
+        LEFT JOIN lake.RAW.vt_county_lines AS county_names
+        ON df.county_fips = county_names.CNTYGEOID
+        """
+    ).df()
+
+    df_with_geoid["GEOID"] = df_with_geoid["GEOID"].case_when(
+        [
+            (
+                (df_with_geoid["geo_type"] == "county")
+                & (df_with_geoid["GEOID"].isna()),
+                df_with_geoid["county_fips"],
+            ),
+            (
+                (df_with_geoid["geo_type"] == "state")
+                & (df_with_geoid["GEOID"].isna()),
+                "50",
+            ),
+        ]
+    )
+
+    df_with_geoid["county"] = df_with_geoid["county"].case_when(
+        [
+            (
+                (df_with_geoid["geo_type"] == "county")
+                & (df_with_geoid["county"].isna()),
+                df_with_geoid["county_name"],
+            ),
+        ]
+    )
+
+    df_with_geoid["county"] = df_with_geoid["county"].str.title()
+
+    df_with_geoid.rename(columns={"GEOID": "geoid"}, inplace=True)
+    df_with_geoid.drop(columns=["county_name"], inplace=True)
+
+    return df_with_geoid
+
+
 def clean(con: duckdb.DuckDBPyConnection):
     raw_df = read_raw_data(con)
-    # NOTE: Cleaning already included in data fetch --> returning raw dataframe
-    return raw_df
+    df = clean_geo_type(raw_df)
+    df = rename_columns(df)
+    df = add_geoid(con, df)
+    df.columns = df.columns.str.lower()
+
+    return df[
+        [
+            "year",
+            "geoid",
+            "name",
+            "geo_type",
+            "county",
+            "county_fips",
+            "section",
+            "variable",
+            "value",
+            "percent",
+        ]
+    ]
 
 
 def add_to_lake(clean_df: pd.DataFrame, con: duckdb.DuckDBPyConnection):
