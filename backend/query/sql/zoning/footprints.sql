d7d48c9eedc3ff26cf0fafd09803cb13ee9aca32
Isarge05 <iansarge05@gmail.com>
2026-09-22 13:22:20 -0400

    cast building footprints geometry column as GeoJSON

--- backend/query/sql/zoning/footprints.sql
+++ backend/query/sql/zoning/footprints.sql
@@ -1,4 +1,12 @@
 {{ cte_filter_block }}
-SELECT b.*
+SELECT 
+    b.object_id,
+    b.town,
+    b.county,
+    b.geoid, 
+    b.name,
+    b.height_ft,
+    b.building_type,
+    ST_AsGeoJSON(b.geometry) AS geometry,
 FROM VCGI_buildingFootprints_geom AS b
 {{ join_filter_block }}
