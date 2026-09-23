0482b3360327c689ae5a090ab20b607d4023bbae
Isarge05 <iansarge05@gmail.com>
2026-09-23 12:52:01 -0400

    updated snake_case column names in wastewater service areas query

--- backend/query/sql/wastewater/service_area_geo_query.sql
+++ backend/query/sql/wastewater/service_area_geo_query.sql
@@ -10,8 +10,8 @@ FROM (
             'type', 'Feature',
             'geometry', ST_AsGeoJSON(ST_Simplify(g.geometry, 0.0001))::JSON,
             'properties', json_object(
-                'System Name', i.SystemName,
-                'System Owner', i.SystemOwner,
+                'System Name', i.system_name,
+                'System Owner', i.system_owner,
                 'rgba_color', '[44, 160, 44, 180]'::JSON,
                 'tooltip', json_object(
                     '__title__', 'Wastewater Service Areas',
