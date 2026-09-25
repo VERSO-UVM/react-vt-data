a2ee1adbf8ce8533f4ddda05dfbd1a422c03ee67
iansargent <iansargent@users.noreply.github.com>
2026-09-25 18:40:58 +0000

    deploy: 8a972f2bd4275b9378fd2e62d0dc0f08512c7b4c

---
+++ _next/static/t041rAUHONu7oRbipquRd/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fdata-comparison\u002Freports-by-topic\u002F[topic]","\u002Fmapping\u002F[slug]","\u002Fresources\u002Fwhite-papers\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
